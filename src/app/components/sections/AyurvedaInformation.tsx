925b5f0229e693d59726d29a38f2f492676f10ef
Aditya Joshi <joshiaditya.aj07@gmail.com>
2024-04-21 19:10:25 +0530

    fix: minor fixes

--- src/app/components/sections/AyurvedaInformation.tsx
+++ src/app/components/sections/AyurvedaInformation.tsx
@@ -9,14 +9,14 @@ const AyurvedaInformation: FC<AyurvedaInformationProps> = ({}) => {
       <div className="flex flex-col justify-center items-center gap-4 ">
         <div className="flex flex-col justify-center items-center gap-1 relative z-0">
           <h2 className="text-[#3A643B] font-bold text-[3rem]">
-            Discover Ayurveda's Magic With Us
+            Discover Ayurveda&apos;s Magic With Us
           </h2>
           <div className="w-full border-[2px] border-[#C3D0C4] absolute -z-[10] bottom-3"></div>
         </div>
 
         <p className="text-[#666666] max-w-[31%] text-center text-[16px]">
           Ayurvedic treatment aims to balance your body and mind, bringing
-          harmony and vitality. It's like a journey to better health using
+          harmony and vitality. It&apos;s like a journey to better health using
           ancient wisdom, a totally effective approach for a better life.
         </p>
 
